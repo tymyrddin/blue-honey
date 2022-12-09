7c4bf34954e7897b41a709315b2ea4d66d364477
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-12-09 01:34:54 +0100

    Refactoring to readthedocs

--- source/conf.py
+++ source/conf.py
@@ -77,7 +77,7 @@ html_show_copyright = False
 
 # Intersphinx
 intersphinx_mapping = {
-    "blue": ("https://blue.tymyrddin.dev/", None),
-    "blue-siem": ("https://blue.tymyrddin.dev/projects/siem/", None),
+    "blue": ("https://blue.tymyrddin.dev/en/latest/", None),
+    "blue-siem": ("https://blue.tymyrddin.dev/projects/siem/en/latest/", None),
 }
 myst_url_schemes = ["http", "https", ]
